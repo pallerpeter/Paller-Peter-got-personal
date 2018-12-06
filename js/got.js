924d19b2365bf712baa4793771d9fe055ccd8727
pallerpet1402@gmail.com <pallerpet1402@gmail.com>
2018-12-06 16:42:29 +0100

    starting files added

---
+++ js/got.js
@@ -0,0 +1,24 @@
+function getGameOfThronesCharacterDatas(url, callbackFunc) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      callbackFunc(this);
+    }
+  };
+  xhttp.open('GET', url, true);
+  xhttp.send();
+}
+
+function successGetGameOfThronesCharacterDatas(xhttp) {
+  // Nem szabad globálisba kitenni a userDatas-t!
+  var userDatas = JSON.parse(xhttp.responseText);
+  // Innen hívhatod meg a többi függvényed
+}
+
+getGameOfThronesCharacterDatas(
+  './json/got.json',
+  successGetGameOfThronesCharacterDatas
+);
+
+// Live servert használd mindig!!!!!
+/* IDE ÍRD A FÜGGVÉNYEKET!!!!!! NE EBBE AZ EGY SORBA HANEM INNEN LEFELÉ! */
