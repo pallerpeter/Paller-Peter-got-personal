16b61bf87501cdd2e649f726658bfe5747c435d4
pallerpet1402@gmail.com <pallerpet1402@gmail.com>
2018-12-07 23:44:53 +0100

    img's in the body

--- js/got.js
+++ js/got.js
@@ -13,6 +13,7 @@ function successGetGameOfThronesCharacterDatas(xhttp) {
   // Nem szabad globálisba kitenni a userDatas-t!
   var userDatas = JSON.parse(xhttp.responseText);
   // Innen hívhatod meg a többi függvényed
+  namesandportraits(userDatas);
 }
 
 getGameOfThronesCharacterDatas(
@@ -22,3 +23,30 @@ getGameOfThronesCharacterDatas(
 
 // Live servert használd mindig!!!!!
 /* IDE ÍRD A FÜGGVÉNYEKET!!!!!! NE EBBE AZ EGY SORBA HANEM INNEN LEFELÉ! */
+function namesandportraits(userDatas) {
+  var arrayOfCharacters = '';
+  for (var i = 0; i < userDatas.length; i += 1) {
+    arrayOfCharacters += `<div class='profiles'> <img src=/${userDatas[i].portrait}>
+  <div><p class='profileName'>${userDatas[i].name}</p>
+ </div></div> `;
+  }
+  document.querySelector('.container').innerHTML = arrayOfCharacters;
+}
+
+function makeRightSidePanel() {
+  var div2 = document.createElement('div');
+  div2.className = 'rightSidePanel';
+  div2.innerHTML = 'content of rightSidePanel';
+  var main = document.querySelector('main');
+  main.appendChild(div2);
+}
+makeRightSidePanel();
+
+/*
+function getProfilToRightPanel(userDatas) {
+  var photos = document.querySelectorAll('img');
+  photos.addEventListener('click', function () {
+    console.log(photos);
+  });
+}
+*/
