05bf6dd85cb1f931e1b065bb2ee50f39a242042d
pallerpet1402@gmail.com <pallerpet1402@gmail.com>
2018-12-08 23:10:25 +0100

    search field added

--- js/got.js
+++ js/got.js
@@ -14,6 +14,10 @@ function successGetGameOfThronesCharacterDatas(xhttp) {
   var userDatas = JSON.parse(xhttp.responseText);
   // Innen hívhatod meg a többi függvényed
   namesandportraits(userDatas);
+  // getBioToRightPanel(userDatas);
+  getSearchFieldToRightPanel();
+  searchingCharacter(userDatas);
+  inputOnEnter(userDatas);
 }
 
 getGameOfThronesCharacterDatas(
@@ -33,20 +37,69 @@ function namesandportraits(userDatas) {
   document.querySelector('.container').innerHTML = arrayOfCharacters;
 }
 
+var div2 = document.createElement('div');
+var divOfCharacter = document.createElement('div');
+var divOfSearch = document.createElement('div');
+
+
 function makeRightSidePanel() {
-  var div2 = document.createElement('div');
   div2.className = 'rightSidePanel';
-  div2.innerHTML = 'content of rightSidePanel';
   var main = document.querySelector('main');
   main.appendChild(div2);
+  div2.appendChild(divOfCharacter);
+  divOfCharacter.innerHTML = 'Valar Morghulis';
+  divOfCharacter.className = 'divOfCharacterClass';
+
+  div2.appendChild(divOfSearch);
+  divOfSearch.className = 'divOfSearchClass';
+  // div2.innerHTML = 'content of rightSidePanel';
 }
 makeRightSidePanel();
 
 /*
-function getProfilToRightPanel(userDatas) {
-  var photos = document.querySelectorAll('img');
-  photos.addEventListener('click', function () {
-    console.log(photos);
+function getProfilToRightPanel() {
+  currentProfile = document.querySelector('div');
+  console.log(currentProfile);
+}
+getProfilToRightPanel();
+*/
+
+function getSearchFieldToRightPanel() {
+  var searchField = document.createElement('input');
+  divOfSearch.appendChild(searchField);
+
+  searchField.type = 'text';
+  searchField.className = 'searchFieldClass';
+  searchField.placeholder = 'keresés';
+  searchField.value = '';
+  console.log(document.querySelector('.searchFieldClass'));
+}
+
+function inputOnEnter(userDatas) {
+  document.querySelector('.searchFieldClass').addEventListener('keypress', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) {
+      searchingCharacter(userDatas);
+    }
   });
 }
-*/
+
+function searchingCharacter(userDatas) {
+  console.log(document.querySelector('.searchFieldClass'));
+  if (document.querySelector('.searchFieldClass').value !== null) {
+    var keresett = document.querySelector('.searchFieldClass').value;
+    for (var i = 0; i < userDatas.length; i++) {
+      if (userDatas[i].name === keresett) {
+        divOfCharacter.innerHTML = userDatas[i].bio;
+        return;
+      }
+      // console.log(searchField.value);
+    }
+  }
+}
+
+/*
+function getBioToRightPanel(userDatas) {
+var keresett = 'Jon Snow';
+for (let i = 0; i < userDatas.length; i++) { if (userDatas[i].name===keresett) { console.log(userDatas[i].bio);
+  document.querySelector('.rightSidePanel').innerHTML=userDatas[i].bio; } } } */
