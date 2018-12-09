2f2ad372cfaaf7448fe6226d7d802009e0c25c5c
pallerpet1402@gmail.com <pallerpet1402@gmail.com>
2018-12-09 23:28:08 +0100

    after fourth version out of time, non-usable

--- js/got.js
+++ js/got.js
@@ -13,11 +13,7 @@ function successGetGameOfThronesCharacterDatas(xhttp) {
   // Nem szabad globálisba kitenni a userDatas-t!
   var userDatas = JSON.parse(xhttp.responseText);
   // Innen hívhatod meg a többi függvényed
-  namesandportraits(userDatas);
-  // getBioToRightPanel(userDatas);
-  getSearchFieldToRightPanel();
-  searchingCharacter(userDatas);
-  inputOnEnter(userDatas);
+  onlyAliveCharacters(userDatas);
 }
 
 getGameOfThronesCharacterDatas(
@@ -27,79 +23,104 @@ getGameOfThronesCharacterDatas(
 
 // Live servert használd mindig!!!!!
 /* IDE ÍRD A FÜGGVÉNYEKET!!!!!! NE EBBE AZ EGY SORBA HANEM INNEN LEFELÉ! */
-function namesandportraits(userDatas) {
-  var arrayOfCharacters = '';
-  for (var i = 0; i < userDatas.length; i += 1) {
-    arrayOfCharacters += `<div class='profiles'> <img src=/${userDatas[i].portrait}>
-  <div><p class='profileName'>${userDatas[i].name}</p>
- </div></div> `;
+
+function onlyAliveCharacters(userDatas) {
+  var arrayOfAliveCharacters = [];
+  for (var i = 0; i < userDatas.length; i++) {
+    if (userDatas[i].dead !== true) {
+      arrayOfAliveCharacters.push(userDatas[i]);
+    }
   }
-  document.querySelector('.container').innerHTML = arrayOfCharacters;
+  generateElements(arrayOfAliveCharacters);
+  makeRightSidePanel();
+  getSearchFieldToRightPanel();
+  search(arrayOfAliveCharacters);
+  return arrayOfAliveCharacters;
+}
+
+function getContainerElement() {
+  var container = document.querySelector('.container');
+  return container;
+}
+
+function showElementContent(index) {
+  var container = getContainerElement();
+  var div = container.children;
+  var arr = onlyAliveCharacters();
+  divOfBio.innerHTML = arr[index].bio;
+}
+
+function addCustomListenerForDiv(element, index) {
+  element.addEventListener('click', function () {
+    showElementContent(index);
+  });
+}
+
+function generateElements(arrayOfAliveCharacters) {
+  var container = getContainerElement();
+  for (var i = 0; i < arrayOfAliveCharacters.length; i += 1) {
+    if (arrayOfAliveCharacters[i].dead !== true) {
+      var div = document.createElement('div');
+      div.setAttribute('class', 'container_class');
+      div.innerHTML = `<div class='profiles'> <img src=/${arrayOfAliveCharacters[i].portrait}>
+       <div><p class='profileName'>${arrayOfAliveCharacters[i].name}</p>
+      </div></div> `;
+      addCustomListenerForDiv(div, i);
+      container.appendChild(div);
+    }
+  }
 }
 
 var div2 = document.createElement('div');
-var divOfCharacter = document.createElement('div');
+var divOfPicture = document.createElement('div');
+var divOfSymbol = document.createElement('div');
+var divOfBio = document.createElement('div');
+var divOfSearchButton = document.createElement('div');
 var divOfSearch = document.createElement('div');
-
+var searchButton = document.createElement('button');
+searchButton.className = 'searchbutton';
+searchButton.innerHTML = 'search';
 
 function makeRightSidePanel() {
   div2.className = 'rightSidePanel';
   var main = document.querySelector('main');
   main.appendChild(div2);
-  div2.appendChild(divOfCharacter);
-  divOfCharacter.innerHTML = 'Valar Morghulis';
-  divOfCharacter.className = 'divOfCharacterClass';
-
+  div2.appendChild(divOfBio);
+  divOfBio.innerHTML = 'Valar Morghulis';
+  divOfBio.className = 'divOfBioClass';
+  div2.appendChild(divOfSymbol);
+  div2.appendChild(divOfPicture);
+  div2.appendChild(divOfSearchButton);
+  divOfSearchButton.appendChild(searchButton);
   div2.appendChild(divOfSearch);
   divOfSearch.className = 'divOfSearchClass';
-  // div2.innerHTML = 'content of rightSidePanel';
+  divOfSearchButton.className = 'buttonFieldClass';
 }
-makeRightSidePanel();
-
-/*
-function getProfilToRightPanel() {
-  currentProfile = document.querySelector('div');
-  console.log(currentProfile);
-}
-getProfilToRightPanel();
-*/
 
 function getSearchFieldToRightPanel() {
   var searchField = document.createElement('input');
   divOfSearch.appendChild(searchField);
-
   searchField.type = 'text';
   searchField.className = 'searchFieldClass';
   searchField.placeholder = 'keresés';
   searchField.value = '';
-  console.log(document.querySelector('.searchFieldClass'));
 }
 
-function inputOnEnter(userDatas) {
-  document.querySelector('.searchFieldClass').addEventListener('keypress', function (e) {
-    var key = e.which || e.keyCode;
-    if (key === 13) {
-      searchingCharacter(userDatas);
-    }
-  });
-}
-
-function searchingCharacter(userDatas) {
-  console.log(document.querySelector('.searchFieldClass'));
-  if (document.querySelector('.searchFieldClass').value !== null) {
-    var keresett = document.querySelector('.searchFieldClass').value;
-    for (var i = 0; i < userDatas.length; i++) {
-      if (userDatas[i].name === keresett) {
-        divOfCharacter.innerHTML = userDatas[i].bio;
-        return;
-      }
-      // console.log(searchField.value);
+function search(arrayOfAliveCharacters) {
+  var searchText = document.querySelector('.searchFieldClass').value.toLowerCase();
+  for (var key in arrayOfAliveCharacters) {
+    if (arrayOfAliveCharacters[key].toLowerCase() === searchText) {
+      divOfBio.innerHTML = arrayOfAliveCharacters[key].bio;
+      alert(`I found it! Index: ${key}, Data: ${arrayOfAliveCharacters[key]}`);
+      return;
     }
   }
+  alert('Element not found!');
 }
 
-/*
-function getBioToRightPanel(userDatas) {
-var keresett = 'Jon Snow';
-for (let i = 0; i < userDatas.length; i++) { if (userDatas[i].name===keresett) { console.log(userDatas[i].bio);
-  document.querySelector('.rightSidePanel').innerHTML=userDatas[i].bio; } } } */
+function searchButtonEventListener(userDatas) {
+  var element = document.querySelector('.searchButton');
+  element.addEventListener('click', function () {
+    search(userDatas);
+  });
+}
